4c5511bbcb64cd8c5cab78819cef0e73d518e99e
Piotr Szwarc <quarcu@gmail.com>
2018-08-26 13:43:06 +0200

    Show hello and platform

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var http = require('http');
+var os = require('os');
 
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hello World! : " + os.platform);
 
 });
 
