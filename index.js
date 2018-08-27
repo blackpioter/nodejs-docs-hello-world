e1e9590fb296afaf54051a0748def1aaab3457cf
Piotr Szwarc <quarcu@gmail.com>
2018-08-27 18:35:40 +0200

    Devel: hello AZURE

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var process = require('process');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! : " + process.platform);
+    response.end("Hello AZURE! : " + process.platform);
 
 });
 
